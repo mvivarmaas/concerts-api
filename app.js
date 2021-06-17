16595b62fb67c940833c793360e7dfb5b7be769d
Max Vivar-Maas <mvivarmaas@gmail.com>
2021-06-17 13:15:23 -0400

    added root and 404 handlers

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 
 console.log("hello world");
+
+app.get('/',(req, res) => {
+    console.log('user hit root');
+})
+// 404 Error
+app.all('*', (req, res) => {
+    res.status(404).send('404 Error: Resource not found');
+})
+
+app.listen(5000, () => {
+    console.log('App listening on port 5000');
+})
